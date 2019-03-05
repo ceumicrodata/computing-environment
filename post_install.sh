2e051508d094ec1bac1a454358a42789403463be
Olivér Kiss <kissoliver91@gmail.com>
2019-03-05 15:53:19 +0100

    add installation shell script for additional useful tools

---
+++ post_install.sh
@@ -0,0 +1,25 @@
+set -ex
+
+# git
+sudo apt-get install git
+
+# chromium
+sudo apt-get install chromium-browser
+
+# VPN
+sudo apt-get install openconnect
+
+# htop
+sudo apt-get install htop
+
+# sshfs
+sudo apt-get install sshfs
+
+# tree
+sudo apt-get install tree
+
+# pip3
+sudo apt-get install python3-pip
+
+# jupyter
+sudo apt-get install jupyter-core
