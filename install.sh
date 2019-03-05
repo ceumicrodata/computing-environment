915415cb33e484d0d341f8f76363ecd7f239ce81
Olivér Kiss <kissoliver91@gmail.com>
2019-03-05 13:25:55 +0100

    correct typo preventing sublime-merge installation

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+set -ex
+
 # slack
 sudo apt install snapd
 sudo snap install slack --classic
@@ -14,7 +16,7 @@ sudo apt-get install sublime-text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-merg
+sudo apt-get install sublime-merge
 
 # python
 sudo apt-get install build-essential checkinstall
