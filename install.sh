3a5bc40bd8ffd64f34dde4d6618e8ba3b846e1e8
Olivér Kiss <kissoliver91@gmail.com>
2019-03-08 10:47:22 +0100

    remove custom python installation from installation shell script

--- install.sh
+++ install.sh
@@ -17,15 +17,3 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-merge
-
-# python
-sudo apt-get install build-essential checkinstall
-sudo apt-get install libffi-dev
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
-    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-cd /usr/src
-sudo wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
-sudo tar xzf Python-3.7.2.tgz
-cd Python-3.7.2
-sudo ./configure --enable-optimizations
-sudo make altinstall
